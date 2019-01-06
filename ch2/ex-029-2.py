4f5ee30f4f841ec8f82613f28b0797b82c10f2d8
Nadiya Yeroshkina <nadin.yeroshina@gmail.com>
2019-01-05 16:14:24 -0800

    complete task by adding param to define the width of the half diamond

--- ch2/ex-029-2.py
+++ ch2/ex-029-2.py
@@ -6,7 +6,7 @@ def cout(c):
 		raise Exception("cheater!")
 	sys.stdout.write(c)
 
-def pound2(count):
+def print_pound(count):
 	pounds_in_a_row = 0
 
 	while pounds_in_a_row < count:
@@ -14,31 +14,18 @@ def pound2(count):
 		pounds_in_a_row = pounds_in_a_row + 1
 	cout("\n")
 
-	# else :
-		# def pound2(count):
-		# '''A version of 'pound' using loops instead of string multiplication.'''
-		# pounds_in_a_row = 0
-		# while pounds_in_a_row < count:
-		# 	cout("#")
-		# 	pounds_in_a_row = pounds_in_a_row + 1
+def solution(width):
+	pound = print_pound	
+	row = 1
 
-
-def solution(row):
-	pound = pound2	
-	row = row
-	# if row < 5:
-
-	while row < 5:
+	while row < width:
 		pound(row)
 		row = row + 1
 
-	# else:		
-	# when row >= 5 
 	while row > 0:
 		pound(row)
 		row = row - 1	
 	
-
 solution(15)
 
 
